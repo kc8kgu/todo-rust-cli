79422827934e200e2bf7f2f363cae5655ef7e4e8
Daniel Carter <kc8kgu@gmail.com>
2026-07-04 18:28:06 -0400

    first working version

--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,136 @@
 use std::env;
-use std::process;
+use std::fs::File;
+use std::io::{self, BufReader, BufWriter};
+use serde::{Deserialize, Serialize};
 
+const PATH: &str = "todo.json";
 
-struct Obj {
-    name: String,
-    value: String
+#[derive(Serialize, Deserialize)]
+struct Todo {
+    id: u64,
+    done: bool,
+    title: String,
 }
-
-// static PATH: &str = "./todo.csv";
 
 fn main() {
 
     let argv: Vec<String> = env::args().collect();
 
-    if argv.len() == 1 {
-        usage();
-        process::exit(0);
-    } else if argv.len() == 2 && argv[1] == "list" {
-        list_todo();
+     if argv.len() == 2 && argv[1] == "list" {
+        list_todos();
     } else if argv.len() == 3 && argv[1] == "add" {
         add_todo(&argv[2]);
+        println!("ok");
     } else if argv.len() == 4 && argv[1] == "edit"{
         edit_todo(&argv[2], &argv[3]);
+        println!("ok");
     } else if argv.len() == 3 && argv[1] == "done" {
         done_todo(&argv[2]);
+        println!("ok");
     } else if argv.len() == 3 && argv[1] == "delete" {
         delete_todo(&argv[2]);
+        println!("ok");
     } else if argv.len() == 2 && argv[1] == "clear" {
-        clear_todo();
+        clear_todos();
+        println!("ok");
     } else {
-        println!("Incorrect parameters or unknown command: {}.", argv[1]);
         usage();
     }
-
-    process::exit(0);
 }
 
 fn usage() {
-    println!("Usage: td <list|add|edit|done|delete> <id> <title>");
+    println!("Usage: td < list | add | edit | done | delete> <id> <title>");
 }
 
-fn list_todo() {
-    println!("listing todos");
+fn list_todos() {
 
-    let todos: Vec<Vec<Obj>> = load_todos();
+    let todos: Vec<Todo> = load_todos();
 
     for todo in todos {
-        for obj in todo {
-            println!("{}:{}", obj.name, obj.value);
-        }
+        println!("{}, {}, {}", todo.id, todo.done, todo.title);
     }
+
 }
 
-fn add_todo(_title: &String) {
-    println!("adding todos");
+fn add_todo(title: &String) {
+    let mut todos = load_todos();
+    
+    let mut max_id = 0;
+    for todo in &todos {
+        if todo.id > max_id {
+            max_id = todo.id;
+        }
+    }
+
+    let next_id = max_id + 1;
+    let new_todo = Todo {id: next_id, done: false, title: title.clone()};
+    
+    todos.push(new_todo);
+    save_todos(&todos);
 }
 
 fn edit_todo(_id: &String, _title: &String) {
-    println!("editing todos");}
+
+    let mut todos = load_todos();
+    let id = _id.parse::<u64>().unwrap();
+
+    for todo in &mut todos {
+        if todo.id == id {
+            todo.title = _title.clone();
+            break;
+        }
+    }
+
+    save_todos(&todos);
+}
 
 fn done_todo(_id: &String) {
-    println!("done todos");}
+
+    let mut todos = load_todos();
+    let id = _id.parse::<u64>().unwrap();
+
+    for todo in &mut todos {
+        if todo.id == id {
+            todo.done = !todo.done;
+            break;
+        }
+    }
+
+    save_todos(&todos);
+}
 
 fn delete_todo(_id: &String) {
-    println!("deleting todos");}
 
-fn clear_todo() {
-    println!("clearing todos");
+    let mut todos = load_todos();
+    let id = _id.parse::<u64>().unwrap();
+
+    for (index, todo) in &mut todos.iter().enumerate() {
+        if todo.id == id {
+            todos.remove(index);
+            break;
+        }
+    }    
+    
+    save_todos(&todos);
 }
 
-fn load_todos() -> Vec<Vec<Obj>> {
-    println!("loading todos");
+fn clear_todos() {
 
-    let todos: Vec<Vec<Obj>> = Vec::new();
-    //let mut todo: Vec<Obj> = Vec::new();
+    let todos = Vec::new();
+    save_todos(&todos);
+}
+
+fn load_todos() -> Vec<Todo> {
+
+    let reader: BufReader<File> = BufReader::new(File::open(PATH).unwrap());
+    let todos: Vec<Todo> = serde_json::from_reader(reader).unwrap();
 
     todos
 }
 
-/*
-fn load_todos_test() -> Vec<Vec<Obj>>{
-    println!("loading todos");
 
-    let mut todos: Vec<Vec<Obj>> = Vec::new();
+fn save_todos(todos: &Vec<Todo>) {
 
-    let mut todo: Vec<Obj> = Vec::new();
-    todo.push( Obj { name: "id".to_string(), value: "0".to_string() } );
-    todo.push( Obj { name: "done".to_string(), value: "0".to_string() } );
-    todo.push( Obj { name: "title".to_string(), value: "check in to git".to_string() } );
-    todos.push(todo);
-
-    let mut todo: Vec<Obj> = Vec::new();
-    todo.push( Obj { name: "id".to_string(), value: "0".to_string() } );
-    todo.push( Obj { name: "done".to_string(), value: "0".to_string() } );
-    todo.push( Obj { name: "title".to_string(), value: "check in to git".to_string() } );
-    todos.push(todo);
-
-
-    todos
+    let writer: BufWriter<File> = io::BufWriter::new(File::create("todo.json").unwrap());
+    serde_json::to_writer(writer, todos).unwrap();
 }
-*/
-
-/*
-fn save_todos() {
-    println!("saving todos");
-}
-*/
